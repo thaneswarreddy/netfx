1020fba46151a504063e39a8095f3a0525a674b7
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-04-07 20:59:44 +0000

    Added Moq and Mvc binaries. Updated properties for Lib (removed Moq source dependency). Added initial version (missing tests) for the MVC ControllerExtensions.

---
+++ trunk/Source/Web/Mvc/ControllerExtensions.cs
@@ -0,0 +1,65 @@
+﻿/* 
+ * Dependencies: 
+ *		System.Web.Abstractions
+ *		System.Web.Mvc
+ *		System.Web.Routing
+ *		
+ * Authors: Daniel Cazzulino - daniel@cazzulino.com
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Web.Routing;
+
+namespace System.Web.Mvc
+{
+	/// <summary>
+	/// Extensions to <see cref="Controller"/> classes.
+	/// </summary>
+	public static class ControllerExtensions
+	{
+		/// <summary>
+		/// Performs a redirect based on an expression representing an 
+		/// invocation to a controller method that may include arguments.
+		/// </summary>
+		/// <typeparam name="T">Type of the controller to redirect to. Can be omitted as it can be inferred from the action type.</typeparam>
+		/// <param name="controller">The controller performing the redirect.</param>
+		/// <param name="action">The action containing the redirect.</param>
+		public static void RedirectToAction<T>(this Controller controller, Expression<Action<T>> action)
+		{
+			MethodCallExpression call = action.Body as MethodCallExpression;
+			if (call == null)
+			{
+				throw new InvalidOperationException("Expression must be a method call");
+			}
+			if (call.Object != action.Parameters[0])
+			{
+				throw new InvalidOperationException("Method call must target lambda argument");
+			}
+
+			string actionName = call.Method.Name;
+			// TODO: Use better logic to chop off the controller suffix
+			string controllerName = typeof(T).Name;
+			if (controllerName.EndsWith("Controller", StringComparison.OrdinalIgnoreCase))
+			{
+				controllerName = controllerName.Remove(controllerName.Length - 10, 10);
+			}
+
+			var values = LinkBuilder.BuildParameterValuesFromExpression(call);
+			values.Add("action", actionName);
+			values.Add("controller", controllerName);
+
+			VirtualPathData vpd = RouteTable.Routes.GetVirtualPath(controller.ControllerContext, values);
+			string target = null;
+			if (vpd != null)
+			{
+				target = vpd.VirtualPath;
+			}
+
+			controller.HttpContext.Response.Redirect(target);
+		}
+	}
+}
---
+++ trunk/UnitTests/Web/Mvc/ControllerExtensionsFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace NetFx.UnitTests.Web.Mvc
+{
+	[TestFixture]
+	public class ControllerExtensionsFixture
+	{
+		[ExpectedException(typeof(InvalidOperationException))]
+		[Test]
+		public void ShouldThrowIfRedirectWrongTarget()
+		{
+			var controller = new FooController();
+			controller.RedirectToAction((BarController c) => Console.WriteLine(c.Value));
+		}
+
+		[Test]
+		public void ShouldRedirectWithNoParameters()
+		{
+			// TODO
+		}
+		
+		public class FooController : Controller
+		{
+		}
+
+		public class BarController : Controller
+		{
+			public event EventHandler Handler;
+			public string Value { get; set; }
+		}
+	}
+}
