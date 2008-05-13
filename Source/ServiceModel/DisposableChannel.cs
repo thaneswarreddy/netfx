8294037ac05cced17cca8d16b28adcd5854032c3
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-05-13 03:27:15 +0000

    Added DisposableChannel

---
+++ Source/ServiceModel/DisposableChannel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.ServiceModel
+{
+	/// <summary>
+	/// Solves the problem where disposing an 
+	/// <see cref="IClientChannel"/> created using 
+	/// a <see cref="ChannelFactory{T}"/> throws 
+	/// if disposing it when a connection error happened.
+	/// </summary>
+	/// <typeparam name="T">Service interface type.</typeparam>
+	public class DisposableChannel<T> : IDisposable
+	{
+		private static ChannelFactory<T> channelFactory;
+		private T channel;
+
+		static DisposableChannel()
+		{
+			channelFactory = new ChannelFactory<T>("*");
+		}
+
+		public DisposableChannel()
+		{
+			channel = channelFactory.CreateChannel();
+		}
+
+		public T Channel
+		{
+			get { return channel; }
+		}
+
+		void IDisposable.Dispose()
+		{
+			IClientChannel clientChannel = channel as IClientChannel;
+			if (clientChannel != null)
+			{
+				if (clientChannel.State == CommunicationState.Opened)
+					clientChannel.Dispose();
+				else
+					clientChannel.Abort();
+			}
+		}
+	}
+}
