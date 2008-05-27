0562fdc1531c03d64f3596d554d8509f6c672106
cibrax <cibrax@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-05-27 16:53:47 +0000

    Added classes to mock the WCF operation context.

---
+++ Source/ServiceModel/IServiceSecurityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Security.Principal;
+using System.IdentityModel.Policy;
+
+namespace System.ServiceModel
+{
+	public interface IServiceSecurityContext
+	{
+		AuthorizationContext AuthorizationContext { get; }
+		ReadOnlyCollection<IAuthorizationPolicy> AuthorizationPolicies { get; }
+		bool IsAnonymous { get; }
+		IIdentity PrimaryIdentity { get; }
+		WindowsIdentity WindowsIdentity { get; }
+	}
+}
---
+++ Source/ServiceModel/ServiceSecurityContextWrapper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.ServiceModel
+{
+	public class ServiceSecurityContextWrapper : IServiceSecurityContext
+	{
+		ServiceSecurityContext context;
+
+		public ServiceSecurityContextWrapper(ServiceSecurityContext context)
+		{
+			this.context = context;
+		}
+
+		#region IServiceSecurityContext Members
+
+		public System.IdentityModel.Policy.AuthorizationContext AuthorizationContext
+		{
+			get
+			{
+				return this.context.AuthorizationContext;
+			}
+		}
+
+		public System.Collections.ObjectModel.ReadOnlyCollection<System.IdentityModel.Policy.IAuthorizationPolicy> AuthorizationPolicies
+		{
+			get
+			{
+				return this.context.AuthorizationPolicies;
+			}
+		}
+
+		public bool IsAnonymous
+		{
+			get
+			{
+				return this.context.IsAnonymous;
+			}
+		}
+
+		public System.Security.Principal.IIdentity PrimaryIdentity
+		{
+			get
+			{
+				return this.context.PrimaryIdentity;
+			}
+		}
+
+		public System.Security.Principal.WindowsIdentity WindowsIdentity
+		{
+			get
+			{
+				return this.context.WindowsIdentity;
+			}
+		}
+
+		#endregion
+	}
+}
