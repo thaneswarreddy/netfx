4c69b7c8f3ff4699e7f6602ae746b137bb18e9e7
bcardiff <bcardiff@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-06-17 14:47:13 +0000

    UriExtensions: manipulation of Uri's QueryStrings.

---
+++ UnitTests/Web/UriExtensionsFixture.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using NUnit.Framework;
+
+namespace NetFx.UnitTests.Web
+{
+	[TestFixture]
+	public class UriExtensionsFixture
+	{
+		[Test]
+		public void HasValue()
+		{
+			var uri = new Uri("http://host?a=1&b=2&a=3");
+			Assert.IsTrue(uri.HasValue("a", "1"));
+			Assert.IsTrue(uri.HasValue("b", "2"));
+			Assert.IsTrue(uri.HasValue("a", "3"));
+			Assert.IsFalse(uri.HasValue("a", "2"));
+			Assert.IsFalse(uri.HasValue("c", "42"));
+		}
+
+		[Test]
+		public void OverrideNonExistingArgument()
+		{
+			var uri = new Uri("http://host/?a=1");
+			var uri2 = uri.Override("b", "4");
+			Assert.IsTrue(uri2.HasValue("a", "1"));
+			Assert.IsTrue(uri2.HasValue("b", "4"));
+		}
+
+		[Test]
+		public void OverrideExistingArgument()
+		{
+			var uri = new Uri("http://host/?c=3");
+			var uri2 = uri.Override("c", "4");
+			Assert.IsTrue(uri2.HasValue("c", "4"));
+			Assert.IsFalse(uri2.HasValue("c", "3"));
+		}
+
+		[Test]
+		public void AppendToUriWithoutQueryString()
+		{
+			var uri = new Uri("http://host");
+			var uri2 = uri.Append("c", "4");
+			Assert.IsTrue(uri2.HasValue("c", "4"));
+		}
+
+		[Test]
+		public void AppendToExistingQueryStringArgument()
+		{
+			var uri = new Uri("http://host/?c=5");
+			var uri2 = uri.Append("c", "4");
+			Assert.IsTrue(uri2.HasValue("c", "5"));
+			Assert.IsTrue(uri2.HasValue("c", "4"));
+		}
+
+		[Test]
+		public void RemoveAllOcurrences()
+		{
+			var uri = new Uri("http://host/?c=5&b=1&c=2");
+			var uri2 = uri.Remove("c");
+			Assert.IsFalse(uri2.HasValue("c", "5"));
+			Assert.IsTrue(uri2.HasValue("b", "1"));
+			Assert.IsFalse(uri2.HasValue("c", "2"));
+		}
+
+		[Test]
+		public void RemoveAllQueryString()
+		{
+			var uri = new Uri("http://host/?c=5&b=1&c=2");
+			var uri2 = uri.WithoutQueryString();
+			Assert.AreEqual(new Uri("http://host/"), uri2);
+		}
+	}
+}
