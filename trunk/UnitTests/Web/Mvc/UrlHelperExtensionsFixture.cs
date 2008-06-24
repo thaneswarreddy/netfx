9684247b4fd0be65a932df5715eca22a6393935d
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-06-24 20:32:04 +0000

    Updated moq reference Added url extension

--- trunk/UnitTests/Web/Mvc/UrlHelperExtensionsFixture.cs
+++ trunk/UnitTests/Web/Mvc/UrlHelperExtensionsFixture.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Web.Mvc;
 using NUnit.Framework;
+using System.Web;
+using Moq;
+using System.Web.Routing;
 
 namespace NetFx.UnitTests.Web.Mvc
 {
@@ -8,5 +11,34 @@ namespace NetFx.UnitTests.Web.Mvc
 	public class UrlHelperExtensionsFixture
 	{
 		// TODO: add tests
+		[Ignore]
+		[Test]
+		public void ShouldRenderControllerUrl()
+		{
+			var http = new Mock<HttpContextBase>();
+			http.CallBase = false;
+			var context = new ViewContext(
+				http.Object, 
+				new System.Web.Routing.RouteData(
+					new Route("foo/{id}/{enabled}", 
+						new Mock<IRouteHandler>().Object),
+					new Mock<IRouteHandler>().Object), 
+				new FooController(), 
+				"Foo", 
+				"Bar", 
+				null, 
+				new TempDataDictionary(http.Object));
+
+			var helper = new UrlHelper(context);
+
+			var url = helper.Action<FooController>(c => c.Register("5", false));
+		}
+
+		class FooController : Controller
+		{
+			public void Register(string id, bool enabled)
+			{
+			}
+		}
 	}
 }
