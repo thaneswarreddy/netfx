e9f9935c0ea5c1ef10353e01713dfe920861cb5e
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-06-24 20:17:33 +0000

    Added helper to retrieve an action Url from the urlhelper

---
+++ Source/Web/Mvc/ControllerExpression.cs
@@ -0,0 +1,45 @@
+﻿/* 
+ * Dependencies: 
+ *		System.Web.Abstractions
+ *		System.Web.Mvc
+ *		System.Web.Routing
+ *		ControllerExpressions.cs
+ * Authors: Daniel Cazzulino - daniel@cazzulino.com
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace System.Web.Mvc
+{
+	static class ControllerExpression
+	{
+		public static MethodCallExpression GetMethodCall<T>(Expression<Action<T>> action) where T : Controller
+		{
+			var call = action.Body as MethodCallExpression;
+			if (call == null)
+			{
+				throw new InvalidOperationException("Expression must be a method call");
+			}
+			if (call.Object != action.Parameters[0])
+			{
+				throw new InvalidOperationException("Method call must target lambda argument");
+			}
+			return call;
+		}
+
+		public static string GetControllerName<T>() where T : Controller
+		{
+			// TODO: Use better logic to chop off the controller suffix
+			string controllerName = typeof(T).Name;
+			if (controllerName.EndsWith("Controller", StringComparison.OrdinalIgnoreCase))
+			{
+				controllerName = controllerName.Remove(controllerName.Length - 10, 10);
+			}
+			return controllerName;
+		}
+	}
+}
---
+++ UnitTests/Web/Mvc/UrlHelperExtensionsFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.Mvc;
+using NUnit.Framework;
+
+namespace NetFx.UnitTests.Web.Mvc
+{
+	[TestFixture]
+	public class UrlHelperExtensionsFixture
+	{
+		// TODO: add tests
+	}
+}
