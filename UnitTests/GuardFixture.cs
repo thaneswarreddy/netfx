0d0db9630f2025aa8f0c2b373690ea496b6b4add
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-07-22 09:10:51 +0000

    Added Brian's GuardQ to netfx!!!

---
+++ UnitTests/GuardFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NetFx.UnitTests
+{
+	[TestFixture]
+	public class GuardQFixture
+	{
+		[ExpectedException(typeof(ArgumentNullException), ExpectedMessage = "Value cannot be null.\r\nParameter name: x")]
+		[Test]
+		public void ShouldThrowIfArgumentIsNull()
+		{
+			object x = null;
+			Guard.NotNull(() => x);
+		}
+
+		[ExpectedException(typeof(ArgumentNullException), ExpectedMessage = "Value cannot be null.\r\nParameter name: a.m")]
+		[Test]
+		public void ShouldThrowIfExpressionIsNull()
+		{
+			P a = new P() { m = null };
+			Guard.NotNull(() => a.m);
+		}
+
+		[ExpectedException(typeof(ArgumentNullException), ExpectedMessage = "Value cannot be null.\r\nParameter name: x")]
+		[Test]
+		public void ShouldThrowIfStringArgumentIsNull()
+		{
+			string x = null;
+			Guard.NotNullOrEmpty(() => x);
+		}
+
+		[ExpectedException(typeof(ArgumentException), ExpectedMessage = "Value cannot be null or an empty string.\r\nParameter name: x")]
+		[Test]
+		public void ShouldThrowIfStringArgumentIsEmpty()
+		{
+			string x = "";
+			Guard.NotNullOrEmpty(() => x);
+		}
+
+		[ExpectedException(typeof(ArgumentException), ExpectedMessage = "Value cannot be null or an empty string.\r\nParameter name: a.m")]
+		[Test]
+		public void ShouldThrowIfStringExpressionIsNull()
+		{
+			P a = new P() { m = "" };
+			Guard.NotNullOrEmpty(() => a.m);
+		}
+
+
+		class P { public string m; }
+	}
+}
