188d63aa105833c8b03df7634c1cf7d74d3e8add
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-10-16 20:11:14 +0000

    Added ExtensionAttribute to support extension methods on .NET 2.0 targets

---
+++ Source/ExtensionAttribute.cs
@@ -0,0 +1,12 @@
+﻿/* 
+ * Summary: Link to this file if you need to enable extension methods for 
+ * .NET 2.0 targeted assemblies.
+ * Dependencies: None
+ * Authors: Daniel Cazzulino - daniel@cazzulino.com
+ */
+namespace System.Runtime.CompilerServices
+{
+	internal class ExtensionAttribute : Attribute
+	{
+	}
+}
