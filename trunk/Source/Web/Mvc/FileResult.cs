33e1ea8b876c6f0088c1951600eb0e4f0f3cfdb1
juan.wajnerman <juan.wajnerman@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-10-24 18:50:32 +0000

    FileResult implementation of ActionResult

---
+++ trunk/Source/Web/Mvc/FileResult.cs
@@ -0,0 +1,39 @@
+﻿/* 
+ * Dependencies: 
+ *		System.Web.Abstractions
+ *		System.Web.Mvc
+ *		System.Web.Routing
+ * Authors: Juan Wajnerman - jwajnerman@manas.com.ar
+ */
+
+using System.IO;
+
+namespace System.Web.Mvc
+{
+	public class FileResult : ActionResult
+	{
+		FileInfo file;
+		string contentType;
+
+		public FileResult(string filePath, string contentType)
+			: this(new FileInfo(filePath), contentType)
+		{
+		}
+
+		public FileResult(FileInfo file, string contentType)
+		{
+			this.file = file;
+			this.contentType = contentType;
+		}
+
+		public bool DownloadAsAttachment { get; set; }
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			context.HttpContext.Response.ContentType = contentType;
+			context.HttpContext.Response.AppendHeader("Content-Length", file.Length.ToString());
+			context.HttpContext.Response.AppendHeader("Content-Disposition", "attachment; filename=" + file.Name);
+			context.HttpContext.Response.TransmitFile(file.FullName);
+		}
+	}
+}
