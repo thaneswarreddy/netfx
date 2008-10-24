e2c840d1a28b38669cc87bb271a97d680d38ee01
juan.wajnerman <juan.wajnerman@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-10-24 14:03:01 +0000

    More custom ActionResults (BinaryResult, StatusCodeResult and SyndicationFeedResult)

---
+++ Source/Web/Mvc/StatusCodeResult.cs
@@ -0,0 +1,27 @@
+﻿/* 
+ * Dependencies: 
+ *		System.Web.Abstractions
+ *		System.Web.Mvc
+ *		System.Web.Routing
+ * Authors: Juan Wajnerman - jwajnerman@manas.com.ar
+ */
+
+using System.Net;
+
+namespace System.Web.Mvc
+{
+	public class StatusCodeResult : ActionResult
+	{
+		HttpStatusCode statusCode;
+
+		public StatusCodeResult(HttpStatusCode statusCode)
+		{
+			this.statusCode = statusCode;
+		}
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			context.HttpContext.Response.StatusCode = (int)statusCode;
+		}
+	}
+}
