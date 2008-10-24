cb3f63d0b5360f6850ffc059c07682549cb4e09e
juan.wajnerman <juan.wajnerman@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-10-24 14:03:01 +0000

    More custom ActionResults (BinaryResult, StatusCodeResult and SyndicationFeedResult)

---
+++ trunk/Source/Web/Mvc/BinaryResult.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ * Dependencies: 
+ *		System.Web.Abstractions
+ *		System.Web.Mvc
+ *		System.Web.Routing
+ * Authors: Juan Wajnerman - jwajnerman@manas.com.ar
+ */
+
+namespace System.Web.Mvc
+{
+	public class BinaryResult : ActionResult
+	{
+		byte[] data;
+		string contentType;
+
+		public BinaryResult(string contentType, byte[] data)
+		{
+			this.data = data;
+			this.contentType = contentType;
+		}
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			context.HttpContext.Response.ContentType = contentType;
+			context.HttpContext.Response.BinaryWrite(data);
+		}
+	}
+}
---
+++ trunk/Source/Web/Mvc/SyndicationFeedResult.cs
@@ -0,0 +1,41 @@
+﻿/* 
+ * Dependencies: 
+ *		System.Web.Abstractions
+ *		System.Web.Mvc
+ *		System.Web.Routing
+ *		System.ServiceModel.Web
+ * Authors: Juan Wajnerman - jwajnerman@manas.com.ar
+ */
+
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace System.Web.Mvc
+{
+	public class SyndicationFeedResult : ActionResult
+	{
+		SyndicationFeed feed;
+		string format;
+
+		public SyndicationFeedResult(SyndicationFeed feed, string format)
+		{
+			this.feed = feed;
+			this.format = format;
+		}
+
+		public SyndicationFeedResult(SyndicationFeed feed)
+			: this(feed, "atom")
+		{
+		}
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			context.HttpContext.Response.ContentType = "text/xml";
+			SyndicationFeedFormatter f = format == "atom" ?
+					(SyndicationFeedFormatter)new Atom10FeedFormatter(feed) :
+					(SyndicationFeedFormatter)new Rss20FeedFormatter(feed);
+			using (var writer = XmlWriter.Create(context.HttpContext.Response.Output))
+				f.WriteTo(writer);
+		}
+	}
+}
