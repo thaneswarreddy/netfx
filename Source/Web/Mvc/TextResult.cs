91d1755676778d2298cf60b3bd64a882e11311c0
bcardiff <bcardiff@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-10-28 16:07:50 +0000

    add TextResult to Mvc extensions

---
+++ Source/Web/Mvc/TextResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Web.Mvc
+{
+    /// <summary>
+    /// ActionResult that renders specified text as a result
+    /// </summary>
+    public class TextResult : ActionResult
+    {
+        /// <summary>
+        /// Creates a TextResult using <paramref name="text"/>
+        /// </summary>
+        /// <param name="text"></param>
+        public TextResult(string text)
+        {
+            Text = text;
+        }
+
+        /// <summary>
+        /// Text to be written in the output
+        /// </summary>
+        public string Text { get; private set; }
+        
+        /// <summary>
+        /// <see cref="ActionResult.ExecuteResult"/>
+        /// </summary>
+        /// <param name="context"></param>
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.Write(Text);
+        }
+    }
+}
