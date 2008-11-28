646ebd6884b34788a5c7ef5af52d319d4038d710
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-11-28 16:16:48 +0000

    Added base interfaces for e-mail clients.

---
+++ trunk/Source/Net/Mail/SmtpClientWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Net.Mail
+{
+	/// <summary>
+	/// Implements the <see cref="ISmtpClient"/> interface 
+	/// by inheriting from <see cref="SmtpClient"/>.
+	/// </summary>
+	public sealed class SmtpClientWrapper : SmtpClient, ISmtpClient
+	{
+		void IDisposable.Dispose()
+		{
+		}
+	}
+}
