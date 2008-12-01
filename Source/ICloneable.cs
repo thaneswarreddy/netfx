eb51aefca7c95870cbd4939320cdcf3888a506f1
kzu.net <kzu.net@f0721e01-9c46-0410-83e2-a97ad06868d3>
2008-12-01 14:00:08 +0000

    Added ICloneable generic interface

---
+++ Source/ICloneable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace System
+{
+	/// <summary>
+	/// Generic interface for cloning objects.
+	/// </summary>
+	public interface ICloneable<T>
+	{
+		/// <summary>
+		/// Clones the object.
+		/// </summary>
+		T Clone();
+	}
+}
